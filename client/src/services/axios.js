be7c749b85b85d8a5979739b558926aa325c6c9a
Tushar Pradyumna Bisane <tpbisane7@gmail.com>
2026-06-06 20:22:41 +0530

    initial commit

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "mandal-website-production.up.railway.app",
 });
 
 API.interceptors.request.use((req) => {
